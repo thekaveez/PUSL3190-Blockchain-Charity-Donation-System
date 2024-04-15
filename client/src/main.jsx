0dee3b6d2102de2667cca9eb1f13c401df4ea10b
Piyum Kavesshwara <piyum7020@gmail.com>
2024-04-16 03:23:31 +0530

    done- smart contract

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+
+const root =ReactDom.createRoot(document.getElementById('root'));
+
+root.render(
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+        <Router>
+            <App />
+        </Router>
+    </ThirdwebProvider>
+);
